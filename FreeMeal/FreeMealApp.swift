a48ba2c3c1e8d290cff2b28d199e6a1eb1d0c8bd
Changhyun Kim <kevintt91@gmail.com>
2023-04-24 11:43:19 -0700

    Initial Commit

---
+++ FreeMeal/FreeMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeMealApp.swift
+//  FreeMeal
+//
+//  Created by 김창현 on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FreeMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
